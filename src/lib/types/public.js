54200237b7bcf261586bd7bbfd0aa1a968dfdb58
Kohei Yoshino <kohei.yoshino@gmail.com>
2026-06-28 15:45:19 -0400

    [skip ci] Add JSDoc descriptions to type definitions

--- src/lib/types/public.js
+++ src/lib/types/public.js
@@ -1430,6 +1430,7 @@
  */
 
 /**
+ * Properties for an asset collection.
  * @typedef {object} AssetCollectionProps
  * @property {string} media_folder Internal media folder path for the collection, relative to the
  * project root.
@@ -1683,6 +1684,7 @@
  */
 
 /**
+ * Issue reporting options.
  * @typedef {object} IssueReports
  * @property {string} url URL of the issue reporting endpoint. Default:
  * `https://github.com/sveltia/sveltia-cms/issues/new`.
@@ -1886,6 +1888,7 @@
  */
 
 /**
+ * Custom preview template React component.
  * @typedef {ComponentType<CustomPreviewTemplateProps>} CustomPreviewTemplate
  */
 
@@ -1893,6 +1896,7 @@
 /* eslint-disable jsdoc/require-property-description */
 
 /**
+ * Props for custom field control React components.
  * @typedef {object} CustomFieldControlProps
  * @property {any} value
  * @property {Record<string, any>} field
@@ -1904,10 +1908,12 @@
  */
 
 /**
+ * Custom field control React component.
  * @typedef {ComponentType<CustomFieldControlProps>} CustomFieldControl
  */
 
 /**
+ * Props for custom field preview React components.
  * @typedef {object} CustomFieldPreviewProps
  * @property {any} value
  * @property {Record<string, any>} field
@@ -1920,10 +1926,12 @@
  */
 
 /**
+ * Custom field preview React component.
  * @typedef {ComponentType<CustomFieldPreviewProps>} CustomFieldPreview
  */
 
 /**
+ * Custom field schema definition.
  * @typedef {object} CustomFieldSchema
  * @property {Record<string, any>} properties
  * @see https://decapcms.org/docs/custom-widgets/#registerwidget
